0177507c53113ce5bebe8a4777d3e9f8581303b9
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-11-28 13:01:41 +0300

    Add files via upload

---
+++ button.ornek.46/button.ornek.46/button.ornek.46/Araba.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace button.ornek._46
+{
+    internal class Araba
+    {
+        string marka;
+        string model;
+        int vites;
+        int hiz = 0;
+
+        public string Marka { get => marka; set => marka = value; }
+        public string Model { get => model; set => model = value; }
+        public int Vites { get => vites; set => vites = value; }
+        public int Hiz { get => hiz; set => hiz = value; }
+
+        public Araba(string marka, string model)
+        {
+            this.marka = marka;
+            this.model = model;
+        }
+        public void Hizlan()
+        {
+            Hiz += 5;
+        }
+        public void Yavasla()
+        {
+            Hiz -= 5;
+        }
+        public string KornaCal()
+        {
+            return "Biiiiipppppp";
+        }
+        public string BilgiYazdir()
+        {
+            return "Marka;" + marka + "\n Model:" + model + "\n Vites:" + vites + "\n Hız:" + hiz;
+        }
+
+        
+        
+    }
+}
---
+++ button.ornek.46/button.ornek.46/button.ornek.46/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace button.ornek._46
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Araba bmw;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmw.Hizlan();
+            label1.Text = bmw.BilgiYazdir();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmw = new Araba("BMW", "5.20İ");
+            label1.Text=bmw.BilgiYazdir();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmw.Yavasla();
+            label1.Text = bmw.BilgiYazdir();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(bmw.KornaCal());
+        }
+    }
+}
